c0ea0f8a81f9cc9514a6f04d4e5e94c3a86d57ac
Rene Araujo <raraujo@unap.edu.pe>
2024-01-18 08:13:37 -0500

    migracion

---
+++ database/migrations/2024_01_18_130527_create_webs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webs', function (Blueprint $table) {
+            // php artisan make:migration create_webs_table
+            // php artisan migrate
+            
+            $table->id();
+            $table->string('url', 150);
+            $table->string('notas', 70);
+            $table->decimal('price', 6, 2);
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webs');
+    }
+};
